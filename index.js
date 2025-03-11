8a4207f4d5cd4c3d244d02a64a09b244e5c31844
Chastly Regino <chastly_regino@icloud.com>
2025-03-11 14:56:49 -0500

    HTTP methods requirements comments

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const express = require('express')
 const { logger } = require('./util/logger.js')
+const userService = require('./service/userService.js')
 
-logger.info(`this is how to use logger`)
+
+const app = express()
+const PORT = 3000
+
+//let user = userService.getUsers()
+//logger.info(`this is how to use logger`)
+
+app.use(express.json())
+
+//POST registration
+
+//POST login
+
+//POST ticket submission
+
+//GET previous tickets (employee)
+
+//GET pending tickets (manager)
+
+//PUT approve/deny ticket
